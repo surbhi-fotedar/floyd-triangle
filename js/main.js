51a05180016ca031a858dad675b3ed07a04da608
Surbhi Fotedar <surbhif@CESCHNDES-0108.CESCHN.COM>
2018-12-11 21:06:17 +0530

    floyd triangle is getting diaplayed

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function() {
+
+  var rows = document.getElementById('inputRows'),
+    result = document.getElementById('resultGrp'),
+    n = 1,
+    i,
+    j;
+
+  function displayFloydTriangle() {
+    for (i = 1; i <= rows.value; i++) {
+      for (j = 1; j <= i; j++, n++) {
+        result.innerHTML += ' ' + n;
+      }
+      result.innerHTML += '<br>';
+    }
+  }
+
+  function addEventListeners() {
+    submitBtn = document.getElementById('submitBtn');
+    submitBtn.addEventListener('click', displayFloydTriangle);
+  }
+
+  addEventListeners();
+
+})();
